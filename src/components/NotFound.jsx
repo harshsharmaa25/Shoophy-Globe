e4dc4b58c591cd99565bee3e7d1242f67b8d1f3e
harshsharmaa25 <harsh24aug2001@gmail.com>
2024-10-25 09:40:07 +0530

    updated

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -10,10 +10,10 @@ function NotFound() {
         <>
             <div className="text-center mt-48">
                 <div className="flex justify-center">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMTTIgZYaYAli7QvpOMWskHlfyPH1279vA0g&s" width="100px" height="100px" ></img>
+                    <img src="https://www.dreamstime.com/royalty-free-stock-images-page-not-found-image28300339" width="100px" height="100px" ></img>
                 </div>
                 <h2 className="font-bold">OOPs</h2>
-                <p className="font-bold">{err.status}</p>
+                <p className="font-bold">{err.status}</p>   
                 <p className="font-bold">Page {err.statusText}</p>
                 <p className="font-bold">{err.data}</p><br />
                 <Link to="/">
